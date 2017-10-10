9ed0a97e2dd69e2956dbe5f68dba5bb204274bf6
molthar <32527846+molthar@users.noreply.github.com>
2017-10-10 14:27:19 -0700

    Updated Formatting in L3-L4

--- testing.ps1
+++ testing.ps1
@@ -1,7 +1,7 @@
 #This process is to fully lockout a user from their CUI acounts as well as create a record
 param([string]$Username = "-")
-#Sets parameters to be used throughout script. If a parameter is not correctly specified it will simple cause an error becasue the default value
-#unless otherwise specified is a -
+#Sets parameters to be used throughout script. If a parameter is not correctly specified it will simple cause an error becasue 
+#the default value unless otherwise specified is a -
 echo "  "
 echo "  "
 Start-Transcript -Path "C:\Exit_Notice_Log_Files\log$Username.txt"
