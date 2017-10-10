d197f7f40aafd39c852c0f264e998b4642482140
molthar <32527846+molthar@users.noreply.github.com>
2017-10-10 14:43:48 -0700

    Error corrected
    LN 16 added -Expand after name to separate the sections.

--- testing.ps1
+++ testing.ps1
@@ -1,7 +1,7 @@
 #This process is to fully lockout a user from their CUI acounts as well as create a record
 param([string]$Username = "-")
-#Sets parameters to be used throughout script. If a parameter is not correctly specified it will simple cause an error becasue 
-#the default value unless otherwise specified is a -
+#Sets parameters to be used throughout script. If a parameter is not correctly specified it will simple cause an error becasue
+#the default value, unless otherwise specified is a -
 echo "  "
 echo "  "
 Start-Transcript -Path "C:\Exit_Notice_Log_Files\log$Username.txt"
@@ -9,12 +9,13 @@ Start-Transcript -Path "C:\Exit_Notice_Log_Files\log$Username.txt"
 #users E#
 echo "  "
 echo "  "
-get-aduser $Username -properties Enabled | select sAMAccountName,Enabled,name
+get-ADUser $Username -properties Enabled | select sAMAccountName,name,enabled
 #Gets the users ADAccount information
 echo "  "
 echo "  "
-Get-ADPrincipalGroupMembership $Username | select name
-#Gets the users current group membership and 
+Get-ADPrincipalGroupMembership $Username | select -Expand name 
+#Gets the users current group membership by the name only. -Expand causes it to be seperate from
+#the above command otherwise the groups appear as "grp_name_Gro..." and don't continue
 echo "  "
 echo "  "
 #disable-adaccount $Username
